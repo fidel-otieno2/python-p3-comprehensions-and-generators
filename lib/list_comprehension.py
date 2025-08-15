d5d5cb0572b4b35980de726f9ca30ec8945bd96d
fidel-otieno2 <fidelotieno05@gmail.com>
2025-08-15 14:57:26 +0300

    all together

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    """Return a list of even numbers from the input list."""
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    """Return a list of sentences with exclamation marks added."""
+    return [sentence + "!" for sentence in sentence_list]
